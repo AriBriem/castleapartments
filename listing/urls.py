ba6c220e97f4dd3b06aa82053fe40d136bb30146
Ari Briem <51213171+AriBriem@users.noreply.github.com>
2025-05-06 18:13:30 +0000

    added svg for white logo

--- listing/urls.py
+++ listing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='listing-index')
+    path('', views.index, name='listing-index'),
+    path('listings/<int:listing_id>/', views.get_listing_by_id, name='listing-detail')
 ]
--- listing/views.py
+++ listing/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'listing/index.html', {"show_navbar": True, "show_footer": True})
+
+def get_listing_by_id(request, listing_id):
+    return render(request, 'listing/listing.html', {"listing_id": listing_id})
