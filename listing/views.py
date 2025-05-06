ffa00d125d818e2322c05a78956b99543b2432eb
Ari Briem <51213171+AriBriem@users.noreply.github.com>
2025-05-06 11:37:23 +0000

    added the basic templates and some routing

--- listing/views.py
+++ listing/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'listing/index.html', {"show_navbar": True, "show_footer": False})
