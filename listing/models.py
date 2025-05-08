fe6af39a7e391627f4d90608c63ae58d4b410b51
Midgardsormurinn <ivar.karl.jonsson@gmail.com>
2025-05-08 13:21:02 +0000

    I love Github

--- listing/models.py
+++ listing/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Postcodes(models.Model):
     postcode = models.IntegerField(primary_key=True)
-    country = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
     def __str__(self):
         return self.postcode
 
@@ -30,7 +30,7 @@ class Listings(models.Model):
     postDate = models.DateTimeField()
 
     def __str__(self):
-        return self.listingID
+        return self.address
 
 
 class ListingImage(models.Model):
